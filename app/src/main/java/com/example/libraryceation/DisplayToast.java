7051976016d055b19998545346e31673e3eda304
poojakbarhate <poojakbarhate6598@gmail.com>
2022-05-06 16:02:57 +0530

    Initial Commit

--- app/src/main/java/com/example/libraryceation/DisplayToast.java
+++ app/src/main/java/com/example/libraryceation/DisplayToast.java
@@ -18,8 +18,7 @@ public class DisplayToast extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         display(DisplayToast.this);
     }
-
-    private void display(Context c) {
+    public void display(Context c) {
         Toast.makeText(c,getResources().getString(R.string.BASE_URL), Toast.LENGTH_SHORT).show();
 
     }
