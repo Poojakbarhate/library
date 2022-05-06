e42abc7bfcb8e9929c71416b605e7da30c999d74
poojakbarhate <poojakbarhate6598@gmail.com>
2022-05-06 10:59:59 +0530

    Initial Commit

--- app/src/main/java/com/example/libraryceation/DisplayToast.java
+++ app/src/main/java/com/example/libraryceation/DisplayToast.java
@@ -1,7 +1,11 @@
 package com.example.libraryceation;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.os.Build.VERSION_CODES.BASE;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.BuildCompat;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,6 +15,6 @@ public class DisplayToast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(DisplayToast.this, BuildConfig.COMMON_BASE_URL, Toast.LENGTH_SHORT).show();
+        Toast.makeText(DisplayToast.this,getResources().getString(R.string.BASE_URL), Toast.LENGTH_SHORT).show();
     }
 }
